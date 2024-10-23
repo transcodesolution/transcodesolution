0e38be0a8cd921cf4a4be2e6b1453fc7ea121cd2
vivekkumbhnai <8530vive@gmail.com>
2024-10-23 16:24:20 +0530

    our team page done

--- js/ourPortfolioTab.js
+++ js/ourPortfolioTab.js
@@ -2,18 +2,18 @@ let ourWorkHeading = document.querySelectorAll(".our-portfolio-hading");
 let ourWorkCard = document.querySelectorAll(".our-portfolio-cards-con");
 
 ourWorkCard[0].style.display = "flex";
-ourWorkHeading[0].style.borderBlockEnd = "2px solid #3f3f3f";
+ourWorkHeading[0].style.borderBlockEnd = "2px solid #63388b";
 
 for (let i = 0; i < 4; i++) {
   ourWorkHeading[i].addEventListener("click", () => {
-    ourWorkHeading[i].style.borderBlockEnd = "2px solid #3f3f3f";
+    ourWorkHeading[i].style.borderBlockEnd = "2px solid #63388b";
     ourWorkCard[i].style.display = "flex";
     for (let j = 0; j < 4; j++) {
       if (i == j) {
         continue;
       } else {
         ourWorkCard[j].style.display = "none";
-        ourWorkHeading[j].style.borderBlockEnd = "none";
+        ourWorkHeading[j].style.borderBlockEnd = "transparent";
       }
     }
   });
